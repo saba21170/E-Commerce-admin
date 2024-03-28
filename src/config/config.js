d43af3f349a5788a3977d795b6d2df38fd2faab7
saba21170 <saba.saeed@arhamsoft.org>
2024-03-28 10:14:25 +0500

    added env.example file

--- src/config/config.js
+++ src/config/config.js
@@ -21,7 +21,6 @@ export const ENV = {
     let decryptedData = CryptoJS.AES.decrypt(encryptData, ENV.secret_key);
     let originalText = decryptedData.toString(CryptoJS.enc.Utf8);
     let decryptedObject = JSON.parse(originalText);
-    //console.log(decryptedObject._id,"from config");
     return decryptedObject._id
     
   },
